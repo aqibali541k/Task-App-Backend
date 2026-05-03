6e18cb03516177f181cf2c2add58519b5b6a484b
aqibali541k <aqibali541k@gmail.com>
2026-05-03 09:51:50 -0700

    fixed api integration

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function connectDB() {
 
 // connect on first request
 connectDB();
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -85,7 +85,8 @@ authRouter.post("/login", async (req, res) => {
             user: {
                 id: user._id,
                 name: user.name,
-                email: user.email
+                email: user.email,
+                profilePic: user.profilePic
             }
         });
 
